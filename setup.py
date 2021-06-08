29142a3971e5ffc6751cccee4d63422da7f9f1de
mhenn <mikeianhenning93@gmail.com>
2021-06-08 10:31:03 +0200

    stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(name='plyddl',
-      version='0.0.1',
+      author='mike-henning',
+      author_email='mikeianhenni93@gmail.com',
+      url='https://github.com/mhenn/plyddl/',
+      description='A simple pddl parser supporting most basic pddl features and numeric fluents from pddl2.1',
       install_requires=['ply']
       )
