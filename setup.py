a6c95b8d2d5b96c7edba6efbeb7103cb0fc1ed4f
mhenn <mikeianhenning93@gmail.com>
2021-06-21 18:05:42 +0200

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='plyddl',
       version='0.0.2.1',
       author='mike henning',
-      author_email='mikeianhenni93@gmail.com',
+      author_email='mikeianhenning93@gmail.com',
       url='https://github.com/mhenn/plyddl/',
       project_urls={
             'Homepage': 'https://github.com/mhenn/plyddl/',
